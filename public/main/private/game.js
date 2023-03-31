2eb1b8e07e7003ef6ab39de575e1a78b3a5086cc
im-somnus <somnusanimus@gmail.com>
2023-03-31 02:03:11 +0200

    update

--- public/main/private/game.js
+++ public/main/private/game.js
@@ -1,5 +1,12 @@
 let pokemonList = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 
+let player = {
+    xp: 0,
+    level: 1,
+    money: 0,
+    attacks: 5
+}
+
 // Pokemon health points bar
 const hpBar = document.getElementById("hpBar");
 const yellowZone = hpBar.ariaValueMax / 1.5;
@@ -10,24 +17,16 @@ let hpBarText = document.getElementById("hpBarText");
 // Experience bar
 const xpBar = document.getElementById("xpBar");
 let xpBarText = document.getElementById("xpBarText");
-let xp = 0;
 const xpGain = 10;
 
 // Player level 
 let levelNav = document.getElementById("level");
-let lvl = 1;
 
 // Player money
 let moneyNav = document.getElementById("money");
-let money = 0;
 
 // Player damage
 let damageNav = document.getElementById("damage");
-let dmg = 10;
-
-// Player's pokemon damage
-let pokeDamageNav = document.getElementById("pokeDamage");
-let pokeDmg = 1;
 
 // Pokemon image
 const pokemon = document.getElementById("pokemon");
@@ -35,29 +34,36 @@ const pokemon = document.getElementById("pokemon");
 // Pokemon name
 const pokeName = document.getElementById("pokemonName");
 
-init();
+// Waits for all the html to load before doing this code
+document.addEventListener("DOMContentLoaded", function (e) {
+    if (localStorage.getItem("player") != null) {
+        load();
+    }
 
-// Initiates the game for the first time
-function init() {
+    // Attack every second
+    setInterval(function () {
+        attack();
+    }, 1000);
+
     // Spawn the first pokemon
     spawnPokemon();
 
-    levelNav.innerHTML = "level: " + lvl;
-    moneyNav.innerHTML = "money: " + money;
-    damageNav.innerHTML = "click damage: " + dmg;
+    levelNav.innerHTML = "level: " + player.level;
+    moneyNav.innerHTML = "money: " + player.money;
+    damageNav.innerHTML = "click damage: " + player.attacks;
     hpBarText.innerHTML = "hp: " + hpBar.ariaValueNow + " / " + hpBar.ariaValueMax;
     hpBarText.style.color = "white";
     xpBarText.innerHTML = "xp: " + xpBar.ariaValueNow + " / " + xpBar.ariaValueMax;
     setExperiencePadding();
     setHealthPointsPadding();
     xpBarText.style.color = "black";
+});
 
-}
 
 function attack() {
     if (hpBar.ariaValueNow > 0) {
         startPokemonShake();
-        if (hpBar.ariaValueNow - dmg <= 65) {
+        if (hpBar.ariaValueNow - player.attacks <= 65) {
             hpBarText.style.color = "black";
 
             if (hpBar.ariaValueNow < redZone) {
@@ -67,7 +73,7 @@ function attack() {
                 hpBar.classList = "progress-bar progress-bar progress-bar-animated bg-warning";
             }
         }
-        hpBar.ariaValueNow -= dmg;
+        hpBar.ariaValueNow -= player.attacks;
         hpBarText.innerHTML = "hp: " + hpBar.ariaValueNow + " / " + hpBar.ariaValueMax;
         hpBar.style.width = "" + hpBar.ariaValueNow + "%";
     }
@@ -94,6 +100,7 @@ function pokemonDies() {
     hpBar.classList = "progress-bar progress-bar progress-bar-animated bg-sucess";
     spawnPokemon();
     restoreHP();
+    save();
 }
 
 function getPokemonDrops() {
@@ -102,8 +109,8 @@ function getPokemonDrops() {
 }
 
 function gainMoney() {
-    money += 1;
-    moneyNav.innerHTML = "money: " + money;
+    player.money += 1;
+    moneyNav.innerHTML = "money: " + player.money;
 }
 
 function startPokemonShake() {
@@ -138,7 +145,7 @@ function setHealthPointsPadding() {
     if (hpBar.ariaValueNow == 100) {
         hpBarText.style.paddingLeft = "100px";
     } else {
-        if (hpBar.ariaValueNow - dmg > 9) {
+        if (hpBar.ariaValueNow - player.attacks > 9) {
             hpBarText.style.paddingLeft = "104px";
         } else if (hpBar.ariaValueNow <= 9) {
             hpBarText.style.paddingLeft = "108px";
@@ -149,34 +156,64 @@ function setHealthPointsPadding() {
 
 function gainXP() {
     if (xpBar.ariaValueNow < 100) {
-        xp += xpGain;
-        xpBar.ariaValueNow = xp;
-        xpBarText.innerHTML = "xp: " + xpBar.ariaValueNow + " / " + xpBar.ariaValueMax;
+        player.xp += xpGain;
+        xpBar.ariaValueNow = player.xp;
+        xpBarText.innerHTML = xpBar.ariaValueNow + " / " + xpBar.ariaValueMax;
     }
 
     if (xpBar.ariaValueNow >= 100) {
+        player.level += 1;
+        player.xp = 0;
 
-        lvl += 1;
-        xp = 0;
-
-        levelNav.innerHTML = "level: " + lvl;
+        levelNav.innerHTML = "level: " + player.level;
         xpBar.ariaValueNow = 0;
         xpBarText.innerHTML = "xp: " + xpBar.ariaValueNow + " / " + xpBar.ariaValueMax;
 
-        if (lvl % 5 == 0) {
-
-            dmg += 2;
-            damageNav.innerHTML = "click damage: " + dmg;
-        } else {
-
+        if (player.level % 2 == 0) {
+            player.attacks += 3;
+            damageNav.innerHTML = "click damage: " + player.attacks;
         }
     }
 
-    xpBar.style.width = "" + xp + "%";
+    xpBar.style.width = "" + player.xp + "%";
     setExperiencePadding();
 }
 
 function restoreHP() {
     hpBar.ariaValueNow = hpBar.ariaValueMax;
     hpBar.style.width = "100%";
+}
+
+// Saves the game by writing play to JSON and save it in localStorage			
+var save = function () {
+    localStorage.setItem("player", JSON.stringify(player));
+}
+
+// Loads the game from localStorage and update favIcon to starter
+var load = function () {
+    player = JSON.parse(localStorage.getItem("player"));
+}
+
+function resetConfirmed() {
+    alert("You have reset your profile!");
+
+    player = {
+        xp: 0,
+        level: 1,
+        money: 0,
+        attacks: 5
+    }
+
+    save();
+    location.reload();
+}
+
+var resetProfile = function () {
+    let confirm = prompt("If you wish to reset your profile, please enter \"RESET\":", "");
+
+    if (confirm === null || confirm === "") {
+        alert("Profile was NOT reset!");
+    } else if (confirm === "RESET") {
+        resetConfirmed();
+    }
 }
