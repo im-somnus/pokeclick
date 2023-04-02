7a304479363c0fd3ee7f9ec6a4205692ccf817da
im-somnus <somnusanimus@gmail.com>
2023-04-02 15:16:13 +0200

    Updated environment with the pushes from the private org

--- public/main/private/script.js
+++ public/main/private/script.js
@@ -1,12 +1,15 @@
 // Cursor
-document.getElementById("body").addEventListener("onmousedown", function(e) { document.getElementById("body").style.cursor.URL = "../images/cursor/clickdown.gif"; });
+document.getElementById("body").addEventListener("onmousedown", function (e) { document.getElementById("body").style.cursor.URL = "../images/cursor/clickdown.gif"; });
 
 var cursor = document.getElementsByClassName('clicked')[0]
-cursor.onmousedown = function() {
-    cursor.classList.add('clickdown')
-}
-cursor.onmouseup = function() {
-    cursor.classList.remove('clickdown')
+
+if (cursor !== null && cursor !== undefined) {
+    cursor.onmousedown = function () {
+        cursor.classList.add('clickdown')
+    }
+    cursor.onmouseup = function () {
+        cursor.classList.remove('clickdown')
+    }
 }
 
 // Function that plays the sound of the file url we send as parameter
@@ -15,7 +18,7 @@ function playAudio(url) {
 }
 
 function changeDarkMode() {
-    
+
     var body = document.getElementById("body");
     body.classList.toggle("dark-mode");
 
@@ -36,12 +39,13 @@ function loadDarkMode() {
         var isDarkModeEnabled = darkModeCookie.split("=")[1] === "true";
         var body = document.getElementById("body");
         body.classList.toggle("dark-mode", isDarkModeEnabled);
-        
+
         var toggleCookie = cookies.find(cookie => cookie.startsWith("darkModeToggle="));
-        if (toggleCookie) {
-          var isToggleChecked = toggleCookie.split("=")[1] === "true";
-          var toggle = document.getElementById("dark-mode-toggle");
-          toggle.checked = isToggleChecked;
+
+        if (toggleCookie && document.getElementById("dark-mode-toggle")) {
+            var isToggleChecked = toggleCookie.split("=")[1] === "true";
+            var toggle = document.getElementById("dark-mode-toggle");
+            toggle.checked = isToggleChecked;
         }
     }
 }
